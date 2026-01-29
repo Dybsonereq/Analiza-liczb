b191cc4664dcc5b32ef544307df57e33619b13ea
Dybsonereq <kuba.dybowski29@gmail.com>
2026-01-29 19:28:47 +0100

    zmiany

--- Analiza liczb/Program.cs
+++ Analiza liczb/Program.cs
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-﻿using System;
-=======
-using System;
->>>>>>> 80472fd08d8d2a87cfee73b81a85f23df80e031c
-
 class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
         int[] liczby = Wczytywanie(5);
 
         Wyswietlanie(liczby);
@@ -71,55 +64,3 @@ class Program
             Console.WriteLine($"Liczby nieparzyste: {nieparzyste}");
         }
     }
-
-=======
-        int[] tablica = new int[5];
-        for (int i = 0; i < tablica.Length; i++)
-        {
-            Console.WriteLine($"\nProszę podaj {i + 1} liczbę całkowitą:");
-            while (!int.TryParse(Console.ReadLine(), out tablica[i]))
-            {
-                Console.WriteLine("To nie jest poprawna liczba");
-            }
-        }
-        Console.WriteLine("\nTwoje liczby to:");
-        for (int i = 0; i < tablica.Length; i++)
-        {
-            Console.WriteLine(tablica[i]);
-        }
-
-        int suma = 0;
-        int min = tablica[0];
-        int max = tablica[0];
-        int parzyste = 0;
-        int nieparzyste = 0;
-
-        for (int i = 0; i < tablica.Length; i++)
-        {
-            suma += tablica[i];
-
-            if (tablica[i] > max) max = tablica[i];
-            if (tablica[i] < min) min = tablica[i];
-        }
-
-            for (int i = 0; i < tablica.Length; i++)
-            {
-                if (tablica[i] % 2 == 0)
-                {
-                    parzyste += 1;
-                }
-                if (tablica[i] % 2 != 0)
-                {
-                    nieparzyste += 1;
-                }
-
-            }
-            Console.WriteLine($"\nSuma wszystkich liczb to: {suma}");
-            Console.WriteLine($"Maksymalna wartość to: {max}");
-            Console.WriteLine($"Minimalna wartość to: {min}");
-            Console.WriteLine($"Liczby parzyste: {parzyste}");
-            Console.WriteLine($"Liczby nieparzyste: {nieparzyste}");
-        
-    }
-}
->>>>>>> 80472fd08d8d2a87cfee73b81a85f23df80e031c
